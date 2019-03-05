7ec0d8719d0dd19ea0fcc4e8ac2582f0d94349a1
Yevheniya <yevheniya.zaslavskaya@gmail.com>
2019-03-05 16:38:08 +0200

    add CGRect extension

--- SportsBuff-Demo/SportsBuff-Demo/Common/Extensions/CGRect+Extension.swift
+++ SportsBuff-Demo/SportsBuff-Demo/Common/Extensions/CGRect+Extension.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2019 yzaslavskaya. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension CGRect {
+    func dividedIntegral(fraction: CGFloat, from fromEdge: CGRectEdge) -> (first: CGRect, second: CGRect) {
+        let dimension: CGFloat
+        
+        switch fromEdge {
+        case .minXEdge, .maxXEdge:
+            dimension = self.size.width
+        case .minYEdge, .maxYEdge:
+            dimension = self.size.height
+        }
+        
+        let distance = (dimension * fraction).rounded(.up)
+        var slices = self.divided(atDistance: distance, from: fromEdge)
+        
+        switch fromEdge {
+        case .minXEdge, .maxXEdge:
+            slices.remainder.origin.x += 1
+            slices.remainder.size.width -= 1
+        case .minYEdge, .maxYEdge:
+            slices.remainder.origin.y += 1
+            slices.remainder.size.height -= 1
+        }
+        
+        return (first: slices.slice, second: slices.remainder)
+    }
+}
