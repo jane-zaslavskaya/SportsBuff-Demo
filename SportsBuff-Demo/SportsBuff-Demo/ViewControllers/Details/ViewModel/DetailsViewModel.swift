0084cf81e79ee46ede4aff8b53ed18b7e646bdc7
Yevheniya <yevheniya.zaslavskaya@gmail.com>
2019-03-06 17:30:08 +0200

    restructure project, create view models

---
+++ SportsBuff-Demo/SportsBuff-Demo/ViewControllers/Details/ViewModel/DetailsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  DetailsViewModel.swift
+//  SportsBuff-Demo
+//
+//  Created by Yevheniya Zaslavskaya on 3/6/19.
+//  Copyright © 2019 yzaslavskaya. All rights reserved.
+//
+
+import Foundation
---
+++ SportsBuff-Demo/SportsBuff-Demo/ViewControllers/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewModel.swift
+//  SportsBuff-Demo
+//
+//  Created by Yevheniya Zaslavskaya on 3/6/19.
+//  Copyright © 2019 yzaslavskaya. All rights reserved.
+//
+
+import Foundation
